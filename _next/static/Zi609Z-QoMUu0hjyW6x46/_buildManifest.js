8c72e94dd03bdfc962346c5051046e571b73a863
H Joseph <hjoseph777@gmail.com>
2025-03-04 09:26:39 -0500

    Auto-generated commit [ci skip]

---
+++ _next/static/Zi609Z-QoMUu0hjyW6x46/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/571-a26b57bbf507a9bb.js","static/chunks/pages/index-117cf54efb1d1d31.js"],"/_error":["static/chunks/pages/_error-7ba65e1336b92748.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
